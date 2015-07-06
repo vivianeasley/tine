1017206506357428b49103b4779786aea1f5261e
Robert Easley <destroyallculture@gmail.com>
2015-07-06 09:47:06 -0700

    Built version of tine.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,114 @@
+Number.isInteger = Number.isInteger || function(value) {
+    return typeof value === "number" && 
+           isFinite(value) && 
+           Math.floor(value) === value;
+};
+var SETTINGS = (function ( ) {
+
+  // Public Variables
+  var SE = { };
+
+  // Story Settings
+  SE.speedy      = false;
+  SE.waitSeconds = 1;
+
+  // Private Variables
+
+  // var privateVariable = 1;
+    
+  // Public Functions
+  // SE.init = function ( ) {
+    
+  // };
+
+
+  // Private Functions
+  
+
+
+  return SE;
+}( ));
+
+var TIMER = (function ( ) {
+
+  // Public Variables
+  var TI = { };
+  // TI.moduleProperty = 2;
+
+  // Private Variables
+  var fTimer;
+
+  // var privateVariable = 1;
+    
+  // Public Functions
+  TI.wait = function ( time, callback ) {
+
+    if ( SETTINGS.speedy === true ) {
+
+      window.setTimeout(callback, 10);
+      // callback( );
+      return;
+    }
+
+    var timePassed = 0;
+    var timeTarget = time*1000;
+
+    startTimer( );
+
+    return;
+
+    function startTimer ( ) {
+      fTimer = setTimeout(function(){ timerLoop( ); }, 1000);
+    }
+
+    function timerLoop ( ) {
+      if ( timePassed >= timeTarget ) {
+        callback( );
+        return;
+      }
+      timePassed += 1000;
+      startTimer( );
+    }
+  };
+
+  TI.killTimer = function ( ) {
+    clearTimeout(fTimer);
+  }
+
+  // Private Functions
+
+
+
+  return TI;
+}( ));
+
+var SAVE = (function ( ) {
+
+  // Public Variables
+  var SA = { };
+
+  // Public Variables
+  SA.saveData = [ ];
+
+  // Private Variables
+    
+  // Public Functions
+  SA.updateSave = function ( optionId ) {
+    var id = [];
+    id.push( optionId );
+
+    SA.saveData.push.apply( SA.saveData, id );
+    
+  };
+
+
+  // Private Functions
+  
+
+
+  return SA;
+}( ));
+
 var MAIN = (function ( ) {
 
   // Public Variables
